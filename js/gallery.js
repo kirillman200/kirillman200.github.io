7de3165465be0c8c8f6e0246fb56a95982e0d6ca
kirillman200 <45470751+kirillman200@users.noreply.github.com>
2018-12-16 14:38:00 -0500

    Perfect Thailand

---
+++ js/gallery.js
@@ -0,0 +1,22 @@
+window.onscroll = function() {myFunction(), myFunction1};
+
+var navbar = document.getElementById("myTopnav");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction1() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
